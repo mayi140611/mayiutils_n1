88b4e63c8d7a94afa7f9b82f3ad2dd16a439c27b
ian <yongguiluo@hotmail.com>
2019-08-08 14:09:20 +0800

    sm

--- mayiutils/db/hive_wrapper.py
+++ mayiutils/db/hive_wrapper.py
@@ -9,13 +9,15 @@
 from pyhive import hive
 
 
-def connect_hive(host, port=10000, database='default', username='hive'):
+def connect_hive(host, port=10000, database='default', username='hive', tablename='scores_model'):
     try:
         conn = hive.Connection(host=host, port=port, database=database, username=username)
         cursor = conn.cursor()
-        # cursor.execute('create table if not exists scores_model(model_file string,create_time int)')
-        print('hive连接成功！')
+        if tablename == 'scores_model':
+            cursor.execute('create table if not exists scores_model(company_code string, model_file string,create_time int)')
+        elif tablename == 'case_feedback':  # 案件反馈
+            cursor.execute('create table if not exists case_feedback(company_code string, claim_id string, rules_fired string, scores float, hit tinyint, flag tinyint, create_time int)')
+        print(f'hive表{tablename} 连接成功！')
         return conn, cursor
     except Exception as e:
-        #     raise Exception("hive连接异常！")
-        print('hive连接失败！')
+        print(f'hive连接失败！{str(e)}')
