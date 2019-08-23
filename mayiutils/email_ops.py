b732f21b08d5c38141b6b8feb5ff0c0135fae2b3
ian <yongguiluo@hotmail.com>
2019-08-23 13:58:52 +0800

    email pictures

--- mayiutils/email_ops.py
+++ mayiutils/email_ops.py
@@ -11,7 +11,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-def send_email(subject, text, smtpserver='smtp.163.com',
+def send_email(subject, text, html=None, smtpserver='smtp.163.com',
                username='13585581243@163.com',
                password='aEkBI36saG9Kh',  # 授权码
                sender='13585581243@163.com',
@@ -40,6 +40,47 @@ def send_email(subject, text, smtpserver='smtp.163.com',
     # 构造文字内容
     text_plain = MIMEText(text, 'plain', 'utf-8')
     msg.attach(text_plain)
+
+    # #构造图片链接
+    # sendimagefile=open(r'D:\pythontest\testimage.png','rb').read()
+    # image = MIMEImage(sendimagefile)
+    # image.add_header('Content-ID','<image1>')
+    # image["Content-Disposition"] = 'attachment; filename="testimage.png"'
+    # msg.attach(image)
+
+    # 构造html
+    # 发送正文中的图片:由于包含未被许可的信息，网易邮箱定义为垃圾邮件，报554 DT:SPM ：<p><img src="cid:image1"></p>
+    # html = """
+    # <html>
+    #   <head></head>
+    #   <body>
+    #     <p>Hi!<br>
+    #        How are you?<br>
+    #        Here is the <a href="http://www.baidu.com">link</a> you wanted.<br>
+    #     </p>
+    #   </body>
+    # </html>
+    # """
+    if html:
+        text_html = MIMEText(html, 'html', 'utf-8')
+        text_html["Content-Disposition"] = 'attachment; filename="texthtml.html"'
+        msg.attach(text_html)
+
+    # #构造附件
+    # sendfile=open(r'D:\pythontest\1111.txt','rb').read()
+    # text_att = MIMEText(sendfile, 'base64', 'utf-8')
+    # text_att["Content-Type"] = 'application/octet-stream'
+    # #以下附件可以重命名成aaa.txt
+    # #text_att["Content-Disposition"] = 'attachment; filename="aaa.txt"'
+    # #另一种实现方式
+    # text_att.add_header('Content-Disposition', 'attachment', filename='aaa.txt')
+    # #以下中文测试不ok
+    # #text_att["Content-Disposition"] = u'attachment; filename="中文附件.txt"'.decode('utf-8')
+    # msg.attach(text_att)
+
+
+
+
     # 发送邮件
     smtp = smtplib.SMTP()
     smtp.connect('smtp.163.com')
