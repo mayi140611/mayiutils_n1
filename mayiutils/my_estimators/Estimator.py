887c6ae3e7bdf256c81e1daa437e3fa0d12d767c
ian <yongguiluo@hotmail.com>
2019-12-05 17:30:17 +0800

    sm

--- mayiutils/my_estimators/Estimator.py
+++ mayiutils/my_estimators/Estimator.py
@@ -99,14 +99,16 @@ class Estimator(object):
 class ClsEstimator(Estimator):
     """Classifier Estimator class.
     """
-    def __init__(self, params: dict=None, data_processor: DataProcessor=None):
+    def __init__(self, params: dict=None, data_processor: DataProcessor=None, model_path: str=None):
         """
         Constructs an `Estimator` instance.
         :param params:
         :param data_processor:
+        :param model_path:  模型的路径
         """
         self._params = params
         self._data_processor = data_processor
+        self._model_path = model_path
 
     def model_fn(self):
         return ""
@@ -166,7 +168,7 @@ class ClsEstimator(Estimator):
 
 
 class CatBoostClsEstimator(ClsEstimator):
-    def __init__(self, params, data_processor):
+    def __init__(self, params=None, data_processor=None, model_path=None):
         """
 
         :param params:
@@ -178,12 +180,15 @@ class CatBoostClsEstimator(ClsEstimator):
                 'loss_function': 'CrossEntropy'
             }
         """
-        super().__init__(params=params, data_processor=data_processor)
-        self._model = self.model_fn()
+        super().__init__(params=params, data_processor=data_processor, model_path=model_path)
+        if self._model_path:
+            self.load_model(self._model_path)
+        else:
+            self._model = self.model_fn()
 
     def model_fn(self):
         from catboost import CatBoostClassifier
-        return CatBoostClassifier(**self._params)
+        return CatBoostClassifier(**self._params) if self._params else CatBoostClassifier()
 
     def train(self, plot=True, verbose=True, show_features_importance=True, init_model=None):
         """
@@ -317,13 +322,14 @@ class CatBoostClsEstimator(ClsEstimator):
         dfr = self.evaluate()
         return df_features_importance, dfr
 
-    def test(self, explain=True):
+    def test(self, df_test=None, cat_cols=None, explain=True):
         from catboost import Pool
         import pandas as pd
-        df_test, cat_cols, id_col_name = self._data_processor.test_input_fn()  # id_col_name: 预测的标识列名称
+        if df_test is None:
+            df_test, cat_cols = self._data_processor.test_input_fn()
         test_data = Pool(data=df_test,
                          cat_features=cat_cols)
-        dfr = pd.DataFrame(df_test[id_col_name])
+        dfr = pd.DataFrame(df_test.index)
         y_test_hat = self._model.predict_proba(test_data)[:, 1]
         dfr['score'] = y_test_hat
         dfr['test_label'] = self._model.predict(test_data)
@@ -331,17 +337,17 @@ class CatBoostClsEstimator(ClsEstimator):
         print(s)
         print(f'su sample num：{s.loc[1] if 1 in s else 0}')
         if explain:
-            rr = self.explain(df_test, cat_cols, id_col_name, dfr)
+            rr = self.explain(df_test, cat_cols, dfr)
             return dfr, rr
         return dfr
 
-    def explain(self, df_test, cat_cols, id_col_name, dfr):
+    def explain(self, df_test, cat_cols, dfr):
         """模型解释"""
         from catboost import Pool
         import pandas as pd
         test_data = Pool(data=df_test, cat_features=cat_cols)
         shap_values = self._model.get_feature_importance(test_data, type='ShapValues')
-        dfs = pd.DataFrame(shap_values[:, :-1], columns=df_test.columns, index=df_test[id_col_name])
+        dfs = pd.DataFrame(shap_values[:, :-1], columns=df_test.columns, index=df_test.index)
         dfs_T = dfs.T
         ss = []
         for i in range(dfs_T.shape[1]):
@@ -349,7 +355,214 @@ class CatBoostClsEstimator(ClsEstimator):
         count = 0
         rr = []
         for line in dfr[dfr.test_label == 1].itertuples():
-            rr.append({"id": line[id_col_name], "FS_SC_SCORE": round(line.score, 2),
+            rr.append({"id": line[0], "SCORE": round(line.score, 2),
+                       "EXPLAIN": ','.join(
+                           [f'{i[0]}:{round(i[1], 2)}' for i in list(zip(ss[count].index, ss[count].values))])})
+            count += 1
+        print(rr)
+        return rr
+
+    def save_model(self, model_path):
+        """
+
+        :param model_path: 'catboost_model.dump'
+        :return:
+        """
+        self._model.save_model(model_path)
+
+    def load_model(self, model_path):
+        self._model = self.model_fn().load_model(model_path)
+
+
+class XGBoostClsEstimator(ClsEstimator):
+    def __init__(self, params=None, data_processor=None, model_path=None):
+        """
+
+        :param params:
+            params = {
+                'iterations': iterations,
+                # learning_rate: 0.1,
+                #     'custom_metric': 'AUC',
+                'custom_metric': custom_metric,
+                'loss_function': 'CrossEntropy'
+            }
+        """
+        super().__init__(params=params, data_processor=data_processor, model_path=model_path)
+        if self._model_path:
+            self.load_model(self._model_path)
+        else:
+            self._model = self.model_fn()
+
+    def model_fn(self):
+        from catboost import CatBoostClassifier
+        return CatBoostClassifier(**self._params) if self._params else CatBoostClassifier()
+
+    def train(self, plot=True, verbose=True, show_features_importance=True, init_model=None):
+        """
+
+        :param plot:
+        :param verbose:
+        :param show_features_importance:
+        :param init_model:
+            CatBoost class or string, [default=None]
+            Continue training starting from the existing model.
+            If this parameter is a string, load initial model from the path specified by this string.
+        :return:
+        """
+        from catboost import Pool
+        import pandas as pd
+        features, labels, cat_cols = self._data_processor.train_input_fn()
+        train_data = Pool(data=features,
+                          label=labels,
+                          cat_features=cat_cols)
+        self._model.fit(train_data, plot=plot, verbose=verbose)
+        if show_features_importance:
+            df_features_importance = pd.DataFrame({'name': self._model.feature_names_,
+                                                   'value': self._model.feature_importances_})
+            df_features_importance = df_features_importance.sort_values('value', ascending=False)
+
+            df_features_importance.reset_index(drop=True, inplace=True)
+            print(df_features_importance.head(5))
+            import matplotlib.pyplot as plt
+            fea_ = df_features_importance.sort_values('value')[df_features_importance.value > 0].value
+            fea_name = df_features_importance.sort_values('value')[df_features_importance.value > 0].name
+            plt.figure(figsize=(10, 20))
+            plt.barh(fea_name, fea_, height=0.5)
+            plt.show()
+            return df_features_importance
+
+    def evaluate(self, df_val=None, y_val=None, cat_cols=None):
+        from sklearn.metrics import classification_report
+        from catboost import Pool
+        import pandas as pd
+        if not df_val:
+            df_val, y_val, cat_cols = self._data_processor.eval_input_fn()
+        test_data = Pool(data=df_val,
+                         cat_features=cat_cols)
+        r = self._model.predict(test_data)
+        print(pd.Series(r).value_counts())
+        print(classification_report(y_val, r))
+        dfr = pd.DataFrame(y_val)
+        dfr.columns = ['true_label']
+        y_test_hat = self._model.predict_proba(test_data)[:, 1]
+        dfr['score'] = y_test_hat
+        dfr['predict_label'] = r
+        dfr = dfr.sort_values('score', ascending=False)
+        dfr['order'] = range(1, dfr.shape[0] + 1)
+        print(dfr[dfr.true_label == 1])
+        return dfr
+
+    def train_eval(self, plot=True, verbose=True, show_features_importance=True, init_model=None, use_best_model: bool=True, early_stopping_rounds: int=None):
+        """
+
+        :param plot:
+        :param verbose:
+        :param show_features_importance:
+        :param init_model:
+            CatBoost class or string, [default=None]
+            Continue training starting from the existing model.
+            If this parameter is a string, load initial model from the path specified by this string.
+        :param use_best_model:
+        :param early_stopping_rounds:
+        :return:
+        """
+        from catboost import Pool
+        import pandas as pd
+        features, labels, cat_cols = self._data_processor.train_input_fn()
+        train_data = Pool(data=features,
+                          label=labels,
+                          cat_features=cat_cols)
+        df_val, y_val, cat_cols = self._data_processor.eval_input_fn()
+        val_data = Pool(data=df_val, label=y_val, cat_features=cat_cols)
+        self._model.fit(train_data, eval_set=val_data, plot=plot, verbose=verbose)
+        if show_features_importance:
+            df_features_importance = pd.DataFrame({'name': self._model.feature_names_,
+                                                   'value': self._model.feature_importances_})
+            df_features_importance = df_features_importance.sort_values('value', ascending=False)
+
+            df_features_importance.reset_index(drop=True, inplace=True)
+            print(df_features_importance.head(5))
+            import matplotlib.pyplot as plt
+            fea_ = df_features_importance.sort_values('value')[df_features_importance.value > 0].value
+            fea_name = df_features_importance.sort_values('value')[df_features_importance.value > 0].name
+            plt.figure(figsize=(10, 20))
+            plt.barh(fea_name, fea_, height=0.5)
+            plt.show()
+            return df_features_importance
+
+    def cross_val(self, nfold=3, shuffle=True, stratified=None, plot=True, partition_random_seed: int=14):
+        """
+
+        :param nfold:
+        :param shuffle:
+        :param stratified:
+        :param plot:
+        :param partition_random_seed:
+        :return:
+            cv results : pandas.core.frame.DataFrame with cross-validation results
+            columns are: test-error-mean  test-error-std  train-error-mean  train-error-std
+        """
+        from catboost import Pool, cv
+        import numpy as np
+        features, labels, cat_cols = self._data_processor.cv_input_fn()
+        cv_data = Pool(data=features,
+                          label=labels,
+                          cat_features=cat_cols)
+        cv_result = cv(cv_data, self._params, nfold=nfold, shuffle=shuffle, stratified=stratified, plot=plot, partition_random_seed=partition_random_seed)
+        print('Best validation {} score: {:.2f}±{:.2f} on step {}'.format(
+            self._params['custom_metric'],
+            np.max(cv_result[f'test-{self._params["custom_metric"]}-mean']),
+            cv_result[f'test-{self._params["custom_metric"]}-std'][np.argmax(cv_result[f'test-{self._params["custom_metric"]}-mean'])],
+            np.argmax(cv_result[f'test-{self._params["custom_metric"]}-mean'])
+        ))
+        print('Precise validation {} score: {}'.format(self._params['custom_metric'], np.max(cv_result[f'test-{self._params["custom_metric"]}-mean'])))
+        return cv_result
+
+    def baseline(self):
+        import numpy as np
+        cv_result = self.cross_val()
+        self._params.update({
+            'iterations': np.argmax(cv_result[f'test-{self._params["custom_metric"]}-mean']),
+        })
+        print(self._params)
+        df_features_importance = self.train_eval()
+        dfr = self.evaluate()
+        return df_features_importance, dfr
+
+    def test(self, df_test=None, cat_cols=None, explain=True):
+        from catboost import Pool
+        import pandas as pd
+        if df_test is None:
+            df_test, cat_cols = self._data_processor.test_input_fn()
+        test_data = Pool(data=df_test,
+                         cat_features=cat_cols)
+        dfr = pd.DataFrame(df_test.index)
+        y_test_hat = self._model.predict_proba(test_data)[:, 1]
+        dfr['score'] = y_test_hat
+        dfr['test_label'] = self._model.predict(test_data)
+        s = dfr['test_label'].value_counts()
+        print(s)
+        print(f'su sample num：{s.loc[1] if 1 in s else 0}')
+        if explain:
+            rr = self.explain(df_test, cat_cols, dfr)
+            return dfr, rr
+        return dfr
+
+    def explain(self, df_test, cat_cols, dfr):
+        """模型解释"""
+        from catboost import Pool
+        import pandas as pd
+        test_data = Pool(data=df_test, cat_features=cat_cols)
+        shap_values = self._model.get_feature_importance(test_data, type='ShapValues')
+        dfs = pd.DataFrame(shap_values[:, :-1], columns=df_test.columns, index=df_test.index)
+        dfs_T = dfs.T
+        ss = []
+        for i in range(dfs_T.shape[1]):
+            ss.append(dfs_T.iloc[:, i].copy().sort_values(ascending=False).iloc[:5])
+        count = 0
+        rr = []
+        for line in dfr[dfr.test_label == 1].itertuples():
+            rr.append({"id": line[0], "SCORE": round(line.score, 2),
                        "EXPLAIN": ','.join(
                            [f'{i[0]}:{round(i[1], 2)}' for i in list(zip(ss[count].index, ss[count].values))])})
             count += 1
